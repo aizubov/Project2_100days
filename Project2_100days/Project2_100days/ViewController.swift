91190799d2ec8b855ef477a444e9dd7c2a7c59b5
Alex Zubov <alexanderzubov@icloud.com>
2022-12-11 17:30:19 +0100

    initial_commit_dev

---
+++ Project2_100days/Project2_100days/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2_100days
+//
+//  Created by user226947 on 12/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
